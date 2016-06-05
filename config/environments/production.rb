028da4a61abb87dde707437bf7ae921f950d530e
knopfler81 <knopfler42@yahoo.fr>
2016-06-05 21:11:31 +0200

    config prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,11 +85,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
-  address:               "smtp.gmail.com",
-  port:                  587,
-  user_name:             ENV["EMAIL"],
-  password:              ENV["PASSWORD"]
-  authentication:        "plain",
-  enable_starttls_auto:  true
+    address:               "smtp.gmail.com",
+    port:                  587,
+    user_name:             ENV["EMAIL"],
+    password:              ENV["PASSWORD"]
+    authentication:        "plain",
+    enable_starttls_auto:  true
+    domain:                "heroku.com",
+
 }
   end
