fcae9bd7d5742ab68706fdbf5bf442a4be3b5ee5
knopfler81 <knopfler42@yahoo.fr>
2016-06-06 16:17:43 +0200

    mail

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,13 @@ class Message
   include ActiveModel::Model
   attr_accessor :name, :email, :subject, :content
    validates :name, :email, :subject, :content, presence: true
+
+   def headers
+    {
+      :subject => "My Contact Form",
+      :to => 'knopfler42@gmail.com',
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+
 end
