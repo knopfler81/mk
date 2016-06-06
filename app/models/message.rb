6152e8998dfb59538a9f6b9396182fe2c0ce949b
knopfler81 <knopfler42@yahoo.fr>
2016-06-06 16:19:19 +0200

    cleaned message

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,12 +3,4 @@ class Message
   attr_accessor :name, :email, :subject, :content
    validates :name, :email, :subject, :content, presence: true
 
-   def headers
-    {
-      :subject => "My Contact Form",
-      :to => 'knopfler42@gmail.com',
-      :from => %("#{name}" <#{email}>)
-    }
-  end
-
 end
