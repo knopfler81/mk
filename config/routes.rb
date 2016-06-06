f4f291031ff164e8c71f0ab1bd45f0c3a9428737
knopfler81 <knopfler42@yahoo.fr>
2016-06-06 18:05:52 +0200

    contact form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # match '/messages',     to: 'messages#new',             via: 'get'
   # resources :messages, only: [:new, :create]
 
+  match '/messages',     to: 'contactss#new',             via: 'get'
   resources "contacts", only: [:new, :create]
 
    root "home#index"
