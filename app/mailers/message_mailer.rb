1bbf2fe7e03ac132ffdc0f56548f3a47bf346165
Nelly Duclos <knopfler42@yahoo.fr>
2017-05-09 11:34:18 +0200

    mailer

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,10 +1,10 @@
-# class MessageMailer < ApplicationMailer
-#   # use your own email address here
-#   default :to => "nelly@markinsummerlongago.com"
+class MessageMailer < ApplicationMailer
+  # use your own email address here
+  default :to => "nelly@markinsummerlongago.com"
 
-#   def message_me(msg)
-#     @msg = msg
+  def message_me(msg)
+    @msg = msg
 
-#     mail from: @msg.email, subject: @msg.subject, body: @msg.content
-#   end
-# end
+    mail from: @msg.email, subject: @msg.subject, body: @msg.content
+  end
+end
